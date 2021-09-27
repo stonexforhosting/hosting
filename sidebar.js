71c4337ee18539dfbd95bd4a54b44266c5e51276
stonexforhosting <91271607+stonexforhosting@users.noreply.github.com>
2021-09-27 14:07:18 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -10,10 +10,33 @@ function addSideBarLi() {
   li.appendChild(a);
   ul.appendChild(li);
 }
-var currentScript = $('script').last();
+
 $(document).ready(() => {
-        addSideBarLi();
-        addSideBarLi();
+  var scripts = document.getElementsByTagName('script');
+  var myScript = scripts[ scripts.length - 1 ];
+
+  var queryString = myScript.src.replace(/^[^\?]+\??/,'');
+
+  var params = parseQuery( queryString );
+  
+  addSideBarLi();
+  addSideBarLi();
   debugger;
-  console.log(currentScript);
+  console.log(params);
 });
+
+
+function parseQuery ( query ) {
+   var Params = new Object ();
+   if ( ! query ) return Params; // return empty object
+   var Pairs = query.split(/[;&]/);
+   for ( var i = 0; i < Pairs.length; i++ ) {
+      var KeyVal = Pairs[i].split('=');
+      if ( ! KeyVal || KeyVal.length != 2 ) continue;
+      var key = unescape( KeyVal[0] );
+      var val = unescape( KeyVal[1] );
+      val = val.replace(/\+/g, ' ');
+      Params[key] = val;
+   }
+   return Params;
+}
